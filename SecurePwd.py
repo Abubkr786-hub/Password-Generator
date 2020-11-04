ce592a3415f09e092edfd1faccaf6753c605d2d7
AbubkrSiddique <abubakar100b@gmail.com>
2020-11-04 17:59:12 +0500

    initial commit

--- SecurePwd.py
+++ SecurePwd.py
@@ -1,9 +1,11 @@
 SECURE = (('a', '@'), ('s', '$'), ('i', '1'), ('and', '&'), ('or', '|'), ('o', '0'))
 
+
 def securePwd(password):
     for a,b in SECURE:
         password = password.replace(a, b)
     return password
+
 
 if __name__ == '__main__':
     userInputPwd = input("Enter your password: \n")
